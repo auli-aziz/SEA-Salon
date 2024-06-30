51d76083b367d04c9854238159e108035c982d94
Aulia A Aziz <auliaanugrahaziz@gmail.com>
2024-06-30 15:57:41 +0700

    fix cors policy

--- server/index.js
+++ server/index.js
@@ -5,20 +5,35 @@ const connectDatabase = require("./src/config/db");
 const authRoutes = require("./src/routes/auth");
 const adminRoutes = require("./src/routes/admin");
 const customerRoutes = require("./src/routes/customer");
+const cors = require("cors");
+
 dotenv.config();
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+const allowedOrigins = [
+  'http://localhost:3000', 
+  'https://sea-salon-topaz.vercel.app',
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+}));
+
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
-  next();
-});
 
 app.use("/administrator", adminRoutes);
 app.use("/customer", customerRoutes);
