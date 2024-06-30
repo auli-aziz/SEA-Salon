6c9a3e74040b1cd66b7e005580b8bb422e51bd79
Aulia A Aziz <auliaanugrahaziz@gmail.com>
2024-06-30 15:18:54 +0700

    fix baseURL

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const baseURL = 'https://sea-salon-api.vercel.app/';
+const baseURL = 'https://sea-salon-api.vercel.app';
 
 // https://vitejs.dev/config/
 export default defineConfig({
